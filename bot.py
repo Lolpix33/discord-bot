b67ea37958e3b18beef65349d26fec933c928422
Lolpix33 <mattiagiannoccari@gmail.com>
2026-01-21 17:53:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,9 +138,13 @@ MAIN_GUILD_ID = 1278033707457843314  # ID del tuo server
 
 @bot.check
 async def server_lock(ctx):
+    # Blocca SOLO i DM
     if ctx.guild is None:
         return False
+
+    # Permette solo nel server principale
     return ctx.guild.id == MAIN_GUILD_ID
+
 
 
 # ================= DATI =================
@@ -234,10 +238,12 @@ def punishment_check():
 def owner_or_direttore_check():
     async def predicate(ctx):
         return (
-            ctx.author.id in [PROPRIETARIO_ID, MANAGER_ID] or
-            any(r.id == DIRETTORE_ROLE_ID for r in ctx.author.roles)
+            ctx.author.id == PROPRIETARIO_ID or
+            any(r.id == DIRETTORE_ROLE_ID for r in ctx.author.roles) or
+            any(r.id == MANAGER_ROLE_ID for r in ctx.author.roles)
         )
     return commands.check(predicate)
+
 
 def service_check():
     async def predicate(ctx):
